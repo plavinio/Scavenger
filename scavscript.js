525fe04c87e5fe734e0cf1e350581648b6429c54
plavinio <54049982+plavinio@users.noreply.github.com>
2019-11-14 13:54:04 -0500

    testing scavscript

--- scavscript.js
+++ scavscript.js
@@ -1,9 +1,11 @@
 //Javascript for Scavenger app
 
-let MasterCats = JSON.parse('"outerCats":[{"Location":["fill in"]},{"mainCats":["**"]},{"extend as necessary"}]');
+let MasterCats = JSON.parse('"outerCats":["A":"one", "B":"two", "C":"three"]');
 
 
 //outerCatNames.push(MasterCats.outerCats[cnami]);}     <-- I'll get rid of this when I'm done don't worry (-Paige)
+// "outerCats":[{"Location":["fill in"]},{"mainCats":["**"]},{"extend as necessary"}]
+
 $(document).ready(function() { //When DOM ready
     console.log("outer category names: ");
     for (let cnami in MasterCats.outerCats){console.log("            " + MasterCats.outerCats[cnami]);}
