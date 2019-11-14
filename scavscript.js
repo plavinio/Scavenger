9ba47f2dfae31f85bf97b4c4f004c692c7eaa571
Paige Lavinio <paigelavinio@1x-nat-vl930-172-30-127-14.wireless.umass.edu>
2019-11-14 13:43:04 -0500

    added ready fxn to scavscript

--- scavscript.js
+++ scavscript.js
@@ -3,7 +3,8 @@
 let MasterCats = JSON.parse('"outerCats":[{"Location":["fill in"]},{"mainCats":["**"]},{"extend as necessary"}]');
 
 
-//outerCatNames.push(MasterCats.outerCats[cnami]);}
-
-console.log("outer category names: ");
-for (let cnami in MasterCats.outerCats){console.log("            " + MasterCats.outerCats[cnami]);}
+//outerCatNames.push(MasterCats.outerCats[cnami]);}     <-- I'll get rid of this when I'm done don't worry (-Paige)
+$(document).ready(function() { //When DOM ready
+    console.log("outer category names: ");
+    for (let cnami in MasterCats.outerCats){console.log("            " + MasterCats.outerCats[cnami]);}
+}
