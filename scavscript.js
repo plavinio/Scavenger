9289b25fedf9c0722fd0d80407cc9d2a092e7a5c
Paige Lavinio <paigelavinio@1x-nat-vl930-172-30-127-14.wireless.umass.edu>
2019-11-14 13:27:57 -0500

    preliminary test update to scavscript

--- scavscript.js
+++ scavscript.js
@@ -1,1 +1,9 @@
 //Javascript for Scavenger app
+
+let MasterCats = JSON.parse('{"outerCats":[{"Location":["fill in"]},{"mainCats":["**"]},{"extend as necessary"}]}');
+
+
+//outerCatNames.push(MasterCats.outerCats[cnami]);}
+
+console.log("outer category names: ");
+for (let cnami in MasterCats.outerCats){console.log("            " + MasterCats.outerCats[cnami]);}
