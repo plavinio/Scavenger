9e3a944a49efc2a1fd7a719b428b217133c1cf3f
Elizabeth Dewsnap <edewsnap@umass.edu>
2019-11-15 16:46:23 -0500

    turns out you need jquery to use jquery

--- scavscript.js
+++ scavscript.js
@@ -1,12 +1,35 @@
 //Javascript for Scavenger app
 
-let MasterCats = JSON.parse('"outerCats":{["A":"one", "B":"two", "C":"three"]}');
-
+//let MasterCats = JSON.parse('"outerCats":{["A":"one", "B":"two", "C":"three"]}');
+let cats = [
+    {
+        "main": "urgent",
+        "subs": ["purse/personal bag", "wallet", "keys", "glasses", "perscription", "personal documents", "suitcase", "money", "medical device", "other"]
+    },
+    {
+        "main": "accessories",
+        "subs": ["earring", "bracelet", "necklace", "watch (not smart)", "ring", "pin", "cufflinks", "tie clip", "sunglasses", "other"]
+    },
+    {
+        "main": "tech",
+        "subs": ["phone", "laptop", "smartwatch", "speaker", "tablet", "headphones", "headset", "charger", "powerbank", "adapters/dongles", "mp3", "game console", "stylus", "other"]
+    },
+    {
+        "main": "clothing",
+        "subs": ["shirt", "sweatshirt", "coat/jacket", "scarf", "hat", "gloves/mittens"/*lobster claws*/, "shoes", "headscarf", "balaclava", "workout gear", "other"]
+    },
+    {
+        "main": "academic",
+        "subs": ["textbook", "iClicker", "notebook/binder", "backpack", "project/homework", "library book", "pencil case", "art supplies", "UCard", "other"]
+    }
+];
 
 //outerCatNames.push(MasterCats.outerCats[cnami]);}     <-- I'll get rid of this when I'm done don't worry (-Paige)
 // "outerCats":[{"Location":["fill in"]},{"mainCats":["**"]},{"extend as necessary"}]
 
 $(document).ready(function() { //When DOM ready
     console.log("outer category names: ");
-    for (let cnami in MasterCats.outerCats){console.log("            " + MasterCats.outerCats[cnami]);}
+    for(x in cats){
+        $('<option value='+x.main+'>'+x.main+'</option>').appendTo($("#Cat1"));
+    }
 });      
