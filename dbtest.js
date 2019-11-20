dc026a12025f61827263ba4c26b84fa8bd092e4c
Hayley Lavinio <hayleylavinio@hayleys-mbp.lan>
2019-11-20 18:10:59 -0500

    Database connected (finally)

--- dbtest.js
+++ dbtest.js
@@ -1,14 +1,19 @@
 //let mongo = require('mongodb');
-let MongoClient = require('mongodb').MongoClient;
-//let mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+let mongoose = require('mongoose');
 //let express = require('express');
 const connectionURL = process.env.MONGO_URL;
+console.log("connection url = " + connectionURL);
+console.log("proc.env.M_U = " + process.env.MONGO_URL);
+const dbName = 'test_db';
 const client = new MongoClient(connectionURL, { useNewUrlParser: true });
-client.connect(err => {
-	console.log('Connected...');
-	const collection = client.db("test").collection("devices");
+client.connect(function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected to server");
+	const collection = client.db("sample_weatherdata").collection("data");
 	// perform actions on the collection object
-	let found = db.collection("sample_weatherdata").find({callLetters: "VCSZ"});
+	let found = collection.find({callLetters: "VCSZ"});
 	console.log(found + "\n Hey, look! It actually works!");
 	client.close();
     });
